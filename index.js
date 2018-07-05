045ed217a0744c8d686ab8a380cae03ebb97d63c
Nuno Balbona <nuno@balbona.me>
2018-07-05 18:37:37 +0200

    Auto reconnect to mongodb if first connection fails

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const path = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
-mongoose.connect('mongodb://localhost/valentinecards');
+
+var mongooseConnectWithRetry = function() {
+  mongoose.connect('mongodb://localhostd/valentinecards', {
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+  }, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(mongooseConnectWithRetry, 5000);
+    }
+  });
+};
+mongooseConnectWithRetry();
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
